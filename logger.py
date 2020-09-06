8b6a815cba71e6765475ddf88d64118c0410512b
mountAP <ap44122@gmail.com>
2020-09-06 10:29:36 +0530

    documented logger.py

--- logger.py
+++ logger.py
@@ -4,16 +4,18 @@ from utility import dateToday, weekDayToday, timeNow
 
 
 def init():
+    """ create a new attendance log """
     with open('meetings-log.csv', 'w', newline='') as csvfile:
         log = csv.writer(csvfile, delimiter=',')
         log.writerow(['Date', 'Day', 'Time', 'Subject/Topic', 'Attendance'])
 
 
 def log(meetingName, attendance):
+    """ logs `attendance` ['P'/'A'] aginst `meetingName` with day, date & time """
     if not path.isfile('meetings-log.csv'):
         init()
     with open('meetings-log.csv', 'a', newline='') as csvfile:
-        log = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        log.writerow([
+        logger = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        logger.writerow([
             dateToday(), weekDayToday(), timeNow(), meetingName, attendance
         ])
