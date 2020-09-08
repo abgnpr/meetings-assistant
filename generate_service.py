a03b1a44b78390eea846117d119e549f5c200776
mountAP <ap44122@gmail.com>
2020-09-08 10:06:25 +0530

    system unit generator

--- generate_service.py
+++ generate_service.py
@@ -2,7 +2,7 @@
 
 from os import environ, path, chmod
 
-service_file = 'meetings_assistant.service'
+service_file = 'meetings-assistant.service'
 user = environ.get('USER')
 current_directory = path.dirname(path.realpath(__file__))
 
@@ -10,14 +10,14 @@ with open(service_file, 'w') as sf:
     sf.writelines(
         f'[Unit]\n\
 Description=Meetings Assistant\n\
-After=graphical.target\n\
+After=graphical.target\n\n\
 [Service]\n\
 User={user}\n\
 Type=simple\n\
 ExecStart={current_directory}/main.py\n\
-Restart=on-failure\n\
+Restart=on-failure\n\n\
 [Install]\n\
-WantedBy=multi-user.target\n'
+WantedBy=graphical.target\n'
     )
 
 chmod(service_file, 420)  # 644 in octal
